3b6cbc89777f35b8774e35e69616adf86ffb8377
NoorTushar <tushar.noor.khan@gmail.com>
2024-04-06 15:05:08 +0600

    right sidebar social icons design done

--- src/pages/Shared/LeftSideNav/LeftSideNav.jsx
+++ src/pages/Shared/LeftSideNav/LeftSideNav.jsx
@@ -1,7 +1,9 @@
 const LeftSideNav = () => {
    return (
       <div>
-         <h3 className="text-2xl">Left Side Nav</h3>
+         <div className="p-4 space-y-3 mb-6">
+            <h3 className="text-xl font-semibold">All Categories</h3>
+         </div>
       </div>
    );
 };
--- src/pages/Shared/RightSideNav/RightSideNav.jsx
+++ src/pages/Shared/RightSideNav/RightSideNav.jsx
@@ -1,7 +1,42 @@
+import { FcGoogle } from "react-icons/fc";
+import { FaGithub } from "react-icons/fa";
+import { FaFacebookF, FaInstagram } from "react-icons/fa6";
+import { BsTwitterX } from "react-icons/bs";
+
 const RightSideNav = () => {
    return (
       <div>
-         <h3 className="text-2xl">Right Side Nav</h3>
+         <div className="p-4 space-y-3 mb-6">
+            <h3 className="text-xl font-semibold">Login With</h3>
+            <button className="btn btn-outline w-full">
+               <FcGoogle className="text-xl"></FcGoogle> GOOGLE
+            </button>
+
+            <button className="btn btn-outline w-full">
+               <FaGithub className="text-xl"></FaGithub> GITHUB
+            </button>
+         </div>
+
+         <div className="p-4 mb-6">
+            <h3 className="text-xl font-semibold mb-3">Find Us On</h3>
+            <a
+               href=""
+               className="flex items-center gap-4 border rounded-t-lg p-4"
+            >
+               <FaFacebookF></FaFacebookF>Facebook
+            </a>
+
+            <a href="" className="flex items-center gap-4 border-x p-4">
+               <BsTwitterX></BsTwitterX>Twitter X
+            </a>
+
+            <a
+               href=""
+               className="flex items-center gap-4 border rounded-b-lg p-4"
+            >
+               <FaInstagram></FaInstagram>Instagram
+            </a>
+         </div>
       </div>
    );
 };
