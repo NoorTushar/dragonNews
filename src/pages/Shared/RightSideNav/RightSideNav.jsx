7b31b6b31dd5ba485f020278128210699e14bfa7
NoorTushar <tushar.noor.khan@gmail.com>
2024-04-06 15:26:52 +0600

    right sidebar social icons design done

--- src/pages/Shared/RightSideNav/RightSideNav.jsx
+++ src/pages/Shared/RightSideNav/RightSideNav.jsx
@@ -2,11 +2,14 @@ import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
 import { FaFacebookF, FaInstagram } from "react-icons/fa6";
 import { BsTwitterX } from "react-icons/bs";
+import qZone1 from "../../../assets/qZone1.png";
+import qZone2 from "../../../assets/qZone2.png";
+import qZone3 from "../../../assets/qZone3.png";
 
 const RightSideNav = () => {
    return (
       <div>
-         <div className="p-4 space-y-3 mb-6">
+         <div className=" space-y-3 mb-5">
             <h3 className="text-xl font-semibold">Login With</h3>
             <button className="btn btn-outline w-full">
                <FcGoogle className="text-xl"></FcGoogle> GOOGLE
@@ -17,7 +20,7 @@ const RightSideNav = () => {
             </button>
          </div>
 
-         <div className="p-4 mb-6">
+         <div className="mb-5">
             <h3 className="text-xl font-semibold mb-3">Find Us On</h3>
             <a
                href=""
@@ -36,6 +39,15 @@ const RightSideNav = () => {
             >
                <FaInstagram></FaInstagram>Instagram
             </a>
+         </div>
+
+         <div className="mb-5 ">
+            <div className="bg-gray-100 p-4  rounded-lg">
+               <h3 className="text-xl font-semibold mb-3">Q-Zone</h3>
+               <img src={qZone1} alt="" />
+               <img src={qZone2} alt="" />
+               <img src={qZone3} alt="" />
+            </div>
          </div>
       </div>
    );
